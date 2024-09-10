653f7a12fe7444541602eae446662f64405dec23
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-09-09 20:10:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Odin-Knight-Travails
+ Knight Travails Project for The Odin Project taking input of a starting coordinate and a target one  and then finding a route of moves between them.
---
+++ main.js
@@ -0,0 +1,11 @@
+// Its basic move is two steps forward and one step to the side or one step forward and two steps to the side. It can face any direction.
+
+//visualizer function to add class to the square of the next move and remove class from current square
+
+
+
+
+//function to create array of next set of valid moves, including making sure doesn't go out of bounds. Might be used by a recursive function
+
+
+// function that decides which move, or maybe top 3 moves, go closer to the goal and feeds that new location into the above move finder function. This one might work recursively.
