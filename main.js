c4e8130c7cb1b46c7c8d62a1f6660b2b6a2836d0
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-09-09 22:20:48 -0400

    First commit
    First commit, just has some comments for some of the parts I think it will need and the basic HTML/CSS

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-// Its basic move is two steps forward and one step to the side or one step forward and two steps to the side. It can face any direction.
+// Knight's basic move is two steps forward and one step to the side or one step forward and two steps to the side. It can face any direction. Given a starting position and a target one find shortest number of moves to reach the target.
 
-//visualizer function to add class to the square of the next move and remove class from current square
+// Gameboard coordinates 8 by 8 grid
+/*
+        "0-0" "0-1" "0-2" "0-3" "0-4" "0-5" "0-6" "0-7"
+        "1-0" "1-1" "1-2" "1-3" "1-4" "1-5" "1-6" "1-7"
+        "2-0" "2-1" "2-2" "2-3" "2-4" "2-5" "2-6" "2-7"
+        "3-0" "3-1" "3-2" "3-3" "3-4" "3-5" "3-6" "3-7"
+        "4-0" "4-1" "4-2" "4-3" "4-4" "4-5" "4-6" "4-7"
+        "5-0" "5-1" "5-2" "5-3" "5-4" "5-5" "5-6" "5-7"
+        "6-0" "6-1" "6-2" "6-3" "6-4" "6-5" "6-6" "6-7"
+        "7-0" "7-1" "7-2" "7-3" "7-4" "7-5" "7-6" "7-7"
+*/
+
+
+//visualizer function to add class to the square of the next move and remove class from current square changing their color
 
 
 
 
-//function to create array of next set of valid moves, including making sure doesn't go out of bounds. Might be used by a recursive function
+//function to create array of next set of valid moves, including making sure doesn't go out of bounds. Might be used by a recursive function. Each move being + or - 2 to one coord and + or - 1 to the other without going over 7 or under 0.
 
 
-// function that decides which move, or maybe top 3 moves, go closer to the goal and feeds that new location into the above move finder function. This one might work recursively.
+
+
+
+
+// function that decides which move, or maybe top 3 moves, go closer to the goal and feeds that new location into the above move finder function. This one might work recursively. Not 100% sure how to define what is closer when it is involving two coordinates but it might be as simple as compare x to x and y to y and picking the option with the lowest result of both.
